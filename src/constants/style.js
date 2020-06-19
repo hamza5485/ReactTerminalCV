753d6882737b6e3b2bb6e8e99a831dabd7db44ba
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-06-19 21:05:34 +1000

    global css constants

---
+++ src/constants/style.js
@@ -0,0 +1,12 @@
+export const TEXT = {
+	fontFamily: 'Hack',
+	fontSize: '15px',
+	color: 'white'
+};
+
+export const COMMAND = {
+	fontFamily: 'Hack',
+	fontSize: '15px',
+	fontWeight: 'bold',
+	color: '#49B9C7',
+};
