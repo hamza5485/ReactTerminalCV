3b2a68af7ec2dfd465e4ae858a6d554e508d325f
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-06-20 22:56:18 +1000

    integrated transpiler inside terminal window

--- src/components/app/Window/Window.js
+++ src/components/app/Window/Window.js
@@ -1,49 +1,17 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { TEXT, COMMAND } from '../../../constants/style'
-import { Typography } from '@material-ui/core';
-import profile from '../../../data/profile';
+import { TEXT } from '../../../constants/style'
 import Cursor from '../Cursor/Cursor';
+import TerminalInfo from '../TerminalInfo/TerminalInfo';
+import Transpiler from '../../../transpiler';
 
 const useStyles = makeStyles((theme) => ({
 	window: {
 		backgroundColor: '#000000AB',
 		...TEXT,
 		height: 'calc(100% - 64px)',
-		overflowY: 'scroll'
-	},
-	root: {
-		flexGrow: 1,
-		padding: theme.spacing(2),
-	},
-	cmd: {
-		...COMMAND
-	},
-	text: {
-		...TEXT
-	},
-	img: {
-		border: `2px solid ${COMMAND.color}`,
-		padding: theme.spacing(1),
-		[theme.breakpoints.down('sm')]: {
-			width: '100px',
-			height: '100px'
-		},
-		[theme.breakpoints.up('md')]: {
-			width: '150px',
-			height: '150px'
-		},
-		[theme.breakpoints.up('lg')]: {
-			width: '200px',
-			height: '200px'
-		},
-	},
-	imgMin: {
-		border: `2px solid ${COMMAND.color}`,
-		padding: theme.spacing(1),
-		width: '100px',
-		height: '100px'
+		overflowY: 'scroll',
+		border: '1px solid black'
 	}
 }));
 
@@ -51,68 +19,28 @@ const Window = props => {
 	const classes = useStyles();
 	const [commandList, setCommandList] = React.useState([]);
 	const [cursorList, setCursorList] = React.useState([]);
+	const [count, setCount] = React.useState(1);
+	const transpiler = new Transpiler();
 
 	const handleCommand = com => {
-		let resp = "";
-		if (com  === "log") resp =  commandList.map(obj => `${obj.command} ==> ${obj.response},,, `);
-		else resp = `you entered command: ${com}`;
-		let comObj = { command: com, response: resp };
-		setCommandList([...commandList, comObj]);
-		return resp;
+		let res = transpiler.transpile(com);
+		let command = `${count} ${res.timestamp} ${res.command}`;
+		setCommandList([...commandList, command]);
+		setCount(count + 1);
+		return res.response;
 	};
 
 	const getCursor = () => {
-		return <Cursor newCommand={handleCommand} />
+		return <Cursor key={cursorList.length + 1} newCommand={handleCommand} />
 	};
 
 	useEffect(() => {
 		setCursorList([...cursorList, getCursor()]);
 	}, [commandList]);
 
-	const getInfo = () => {
-		return (
-			<div className={classes.root}>
-				<Grid container spacing={3}>
-					<Grid item xs={3}>
-						<img src='me_terminal.jpeg' alt={`hamzaanas`}
-							className={props.isMax ? classes.img : classes.imgMin} />
-					</Grid>
-					<Grid item xs={9}>
-						<Typography className={classes.cmd}>hamzaanas.dev</Typography>
-						<Typography className={classes.text}>{"----------------"}</Typography>
-						<Grid container>
-							<Grid item>
-								<Typography className={classes.cmd}>{`name`}</Typography>
-							</Grid>
-							<Grid item>
-								<Typography className={classes.text}>{`: ${profile.name}`}</Typography>
-							</Grid>
-						</Grid>
-						<Grid container>
-							<Grid item>
-								<Typography className={classes.cmd}>{`title`}</Typography>
-							</Grid>
-							<Grid item>
-								<Typography className={classes.text}>{`: ${profile.title}`}</Typography>
-							</Grid>
-						</Grid>
-						<Typography className={classes.text}>{"----------------"}</Typography>
-						<Typography className={classes.text}>Lorem ipsum dolor sit amet, consectetur
-						adipiscing elit. Proin hendrerit posuere lorem, eu sagittis sapien egestas in.
-						Vestibulum lacinia purus non eros facilisis finibus. Vivamus sit amet ex ipsum.
-						Sed vel faucibus nulla, eget pharetra sem. Donec ac sem ut lacus posuere
-						malesuada volutpat non arcu. Quisque sit amet tortor molestie, egestas diam
-						rhoncus, varius sapien. Mauris condimentum dolor at purus dapibus, et scelerisque
-					tellus euismod.</Typography>
-					</Grid>
-				</Grid>
-			</div>
-		);
-	};
-
 	return (
 		<div className={classes.window}>
-			{getInfo()}
+			<TerminalInfo isMax={props.isMax} />
 			{cursorList.length > 0 && cursorList.map(elem => elem)}
 		</div>
 	);
