149ea32517877219bd54b4b92dc08240e714ca09
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-06-27 02:48:13 +1000

    added skills section

---
+++ src/transpiler/skills/data.js
@@ -0,0 +1,55 @@
+const skills = {
+	skill: [
+		"Amazon Web Services",
+		"Mobile Development",
+		"Web Development",
+		"Database Management",
+		"VCS",
+		"Linux"
+	],
+	technologies: {
+		AWS: [
+			"EC2",
+			"ELB",
+			"Lambda",
+			"S3",
+			"Route53",
+			"API Gateway",
+			"IAM",
+			"Cognito"
+		],
+		JavaScript: [
+			"NodeJS",
+			"TypeScript",
+			"ReactJS",
+			"Redux",
+			"JQuery"
+		],
+		Python: [
+			"Bottle",
+			"Django"
+		],
+		Java: [
+			"Android Development"
+		],
+		"C#": [
+			".NET Core"
+		],
+		PHP: [
+			"CodeIgniter"
+		],
+		Design: [
+			"Bootstrap",
+			"Material UI",
+			"JSX",
+			"HTML",
+			"CSS"
+		],
+		Database: [
+			"Relational",
+			"NoSQL"
+		]
+	}
+}
+
+export default skills;
