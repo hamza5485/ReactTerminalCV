693a69d042fd6daba5d2e399869cf2e028544c70
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-06-27 02:54:04 +1000

    modified experience section

--- src/transpiler/experience/data.js
+++ src/transpiler/experience/data.js
@@ -2,6 +2,7 @@ const experience = [
 	{
 		title: 'Software Engineer',
 		company: 'Intuitive Technology Innovations Pty Ltd',
+		type: 'Fulltime',
 		location: 'Sydney, Australia',
 		current: true,
 		dates: {
@@ -12,41 +13,56 @@ const experience = [
 	{
 		title: 'Software Engineer',
 		company: 'vAmbition',
+		type: 'Fulltime',
 		location: 'Sydney, Australia',
-		current: true,
+		current: false,
 		dates: {
 			start: 'October 2019',
-			end: null,
+			end: 'Feb 2020',
 		}
 	},
 	{
 		title: 'Software Developer Intern',
 		company: 'ITIC - IT Training & Resourcing',
+		type: 'Internship',
 		location: 'Sydney, Australia',
 		current: false,
 		dates: {
 			start: 'August 2019',
-			end: 	'October 2019',
+			end: 'October 2019',
 		}
 	},
 	{
 		title: 'Android Developer',
 		company: 'Innovation Square Pvt Ltd',
+		type: 'Fulltime',
 		location: 'Islamabad, Pakistan',
 		current: false,
 		dates: {
 			start: 'January 2017',
-			end: 	'July 2019',
+			end: 'July 2019',
 		}
 	},
 	{
-		title: 'Web Developer',
+		title: 'PHP Web Developer',
 		company: 'CyberVision International',
+		type: 'Fulltime',
 		location: 'Islamabad, Pakistan',
 		current: false,
 		dates: {
 			start: 'August 2016',
-			end: 	'December 2016',
+			end: 'December 2016',
+		}
+	},
+	{
+		title: 'PHP Developer Intern',
+		company: 'National Highway Authority',
+		type: 'Internship',
+		location: 'Islamabad, Pakistan',
+		current: false,
+		dates: {
+			start: 'September 2015',
+			end: 'September 2015',
 		}
 	},
 ];
