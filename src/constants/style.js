35eda70fe478dfd499c5d0a0ff395521d61bf449
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-07-04 03:48:41 +1000

    changed font size

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,12 +1,12 @@
 export const TEXT = {
 	fontFamily: 'Hack',
-	fontSize: '15px',
+	fontSize: '13px',
 	color: 'white'
 };
 
 export const COMMAND = {
 	fontFamily: 'Hack',
-	fontSize: '15px',
+	fontSize: '13px',
 	fontWeight: 'bold',
 	color: '#49B9C7 !important',
 };
